2d4f4a62020ae2b00e4cd4477e87fc6cd5671900
jsshuai0 <453024600@qq.com>
2015-01-16 13:01:52 +0800

    删除

--- CommenTest/src/com/anthole/commentest/MainActivity.java
+++ CommenTest/src/com/anthole/commentest/MainActivity.java
@@ -57,7 +57,6 @@ public class MainActivity extends Activity{
         return true;
     }
 
-    @SuppressLint("NewApi") @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
